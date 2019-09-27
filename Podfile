2dbefa2074567c38352e43885c46ca60360cd24f
Lukasz Gajewski <gajewski.lukasz@gmail.com>
2019-09-27 13:38:52 +0200

    cocoapods

---
+++ Podfile
@@ -0,0 +1,8 @@
+
+use_frameworks!
+
+target 'textToSpeech' do
+    pod 'RxSwift', '~> 5.0'
+    pod 'RxCocoa', '~> 5.0'
+    pod 'RealmSwift'
+end
---
+++ Pods/Target Support Files/Pods-textToSpeech/Pods-textToSpeech-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_textToSpeech : NSObject
+@end
+@implementation PodsDummy_Pods_textToSpeech
+@end
